69ea28e7a9dc791d5c5cc9b6d082da090cb8e301
Christophe-33 <chris__33@live.fr>
2020-10-20 09:47:29 +0200

    Utilisation d'un composant en mode onPush (recharge manuelle du composant)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from './person';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'changeDetection';
+
+  public person: Person={
+    name:"john Doe",
+    age : 30
+  }
+
+  public check(){
+    console.log("PARENT view refreshing")
+  }
+
+  public reassign(){
+    this.person={
+      name: this.person.name,
+      age: this.person.age
+    }
+  }
+
 }
